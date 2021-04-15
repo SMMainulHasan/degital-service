18cc4486309ec2ef8d9992b0c28e92ebaa9d9943
SMMainulHasan <mainulhasan7070@gmail.com>
2021-04-16 04:15:01 +0600

    okk

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -4,7 +4,7 @@ import './Services.css';
 const Services = () => {
     return (
         <section className="services">
-            <h3>Services</h3>
+            <h5 className="text-color-primary text-center">Services</h5>
 
         </section>
     );
