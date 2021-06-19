2791124edec0e7452a084b5ed73035e73975e687
Mainul Hasan <mainulhasan7070@gmail.com>
2021-06-19 21:09:56 +0600

    horizental scroll ber fixed

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import './HeaderMain.css';
-import Dream from '../../../Image_Icon/Image/dream.svg'
+import React from "react";
+import Dream from "../../../Image_Icon/Image/dream.svg";
+import "./HeaderMain.css";
 
 const HeaderMain = () => {
-    return (
-            <main className="row d-flex align-items-center banner">
-                <div className="col-md-5 offset-1 tag-line">
-                    <h1>DIGITAL SERVICES <br /> MAKES <br /> DREAMS COME TRUE</h1>
-                    <p><strong>Lorem ipsum dolor sit amet consectetur adipisicing elit.</strong></p>
-                </div>
-                <div className="col-md-6 banner-image">
-                    <img src={Dream} alt="Technical Support" />
-                </div>
-            </main>
-    );
+  return (
+    <main className="row d-flex align-items-center banner">
+      <div className="col-md-5 offset-1 tag-line">
+        <h1>
+          DIGITAL SERVICES <br /> MAKES <br /> DREAMS COME TRUE
+        </h1>
+        <p>
+          <strong>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit...
+          </strong>
+        </p>
+      </div>
+      <div className="col-md-6 banner-image">
+        <img src={Dream} alt="Technical Support" />
+      </div>
+    </main>
+  );
 };
 
 export default HeaderMain;
